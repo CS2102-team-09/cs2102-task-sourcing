3b1e275cf0d1904b1250e29f7a22287e47c8d4f8
caidicaidi123 <caidicaidi123@gmail.com>
2018-10-09 22:28:52 +0800

    logo

--- htdocs/index.php
+++ htdocs/index.php
@@ -12,7 +12,8 @@ if (isset($_SESSION['login_user'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
+          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
 
     <style>
@@ -29,78 +30,76 @@ if (isset($_SESSION['login_user'])) {
     </style>
 </head>
 <body>
-<div class="container">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">TaskSource</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Bid Tasks <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Manage Tasks <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Help <span class="sr-only">(current)</span></a>
+            </li>
+        </ul>
+
+        <button class="btn btn-outline-success my-2 my-sm-0" type="button" data-toggle="modal"
+                data-target="#loginModel">Login
         </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Bid Tasks <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Manage Tasks <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Help <span class="sr-only">(current)</span></a>
-                </li>
-            </ul>
-
-            <button class="btn btn-outline-success my-2 my-sm-0" type="button" data-toggle="modal"
-                    data-target="#loginModel">Login
-            </button>
-        </div>
-    </nav>
-</div>
+    </div>
+</nav>
 
 
 <!-- Modal -->
 <div class="modal fade" id="loginModel">
-    <div class="modal-dialog modal-dialog-centered role="document"">
+    <div class="modal-dialog modal-dialog-centered role=" document
+    "">
 
-        <!-- Modal content-->
-        <div class="modal-content">
-            <div class="modal-header" style="padding:30px 40px;">
-                <h5><span class="fa fa-lock"></span> Login</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-
-
-            <div class="modal-body" style="padding:30px 40px;">
-                <form role="form">
-                    <div class="form-group">
-                        <label for="usrname"><span class="fa fa-user"></span> Username</label>
-                        <input type="text" class="form-control" id="usrname" placeholder="Enter email">
-                    </div>
-                    <div class="form-group">
-                        <label for="psw"><span class="fa fa-eye"></span> Password</label>
-                        <input type="text" class="form-control" id="psw" placeholder="Enter password">
-                    </div>
-                    <div class="checkbox">
-                        <label><input type="checkbox" value="" checked>Remember me</label>
-                    </div>
-
-                </form>
-            </div>
-
-
-            <div class="modal-footer" style="padding:30px 40px;">
-                <button type="submit" class="btn btn-success btn-block"><span class="fa fa-power-off"></span> Login</button>
-            </div>
+    <!-- Modal content-->
+    <div class="modal-content">
+        <div class="modal-header" style="padding:30px 40px;">
+            <h5><span class="fa fa-lock"></span> Login</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
         </div>
 
-    </div>
-</div>
 
+        <div class="modal-body" style="padding:30px 40px;">
+            <form role="form">
+                <div class="form-group">
+                    <label for="usrname"><span class="fa fa-user"></span> Username</label>
+                    <input type="text" class="form-control" id="usrname" placeholder="Enter email">
+                </div>
+                <div class="form-group">
+                    <label for="psw"><span class="fa fa-eye"></span> Password</label>
+                    <input type="text" class="form-control" id="psw" placeholder="Enter password">
+                </div>
+                <div class="checkbox">
+                    <label><input type="checkbox" value="" checked>Remember me</label>
+                </div>
+
+            </form>
+        </div>
+
+
+        <div class="modal-footer" style="padding:30px 40px;">
+            <button type="submit" class="btn btn-success btn-block"><span class="fa fa-power-off"></span> Login</button>
+        </div>
+    </div>
+
+</div>
+</div>
 
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
