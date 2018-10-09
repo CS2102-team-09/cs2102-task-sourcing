fb025548b9431516c062d9fc55ee42b813c6ccb0
Yong Siang Low <lowyongsiang@gmail.com>
2018-10-09 12:57:57 +0800

    Changed user_id domain and extension for schema

---
+++ schema.sql
@@ -0,0 +1,34 @@
+CREATE TABLE users ( 
+	user_id VARCHAR(128) PRIMARY KEY,
+	is_admin BOOL DEFAULT False,
+	password VARCHAR(128) NOT NULL
+);
+
+CREATE TABLE task_managed_by ( 
+	task_id CHAR(8) NOT NULL UNIQUE,
+	user_id VARCHAR(128),
+	status VARCHAR(32),
+	date DATE NOT NULL,
+	start_time NUMERIC,
+	end_time NUMERIC,
+	description VARCHAR(64) NOT NULL, 
+	
+	FOREIGN KEY(user_id) REFERENCES users(user_id)
+		ON UPDATE CASCADE ON DELETE CASCADE,
+	
+	PRIMARY KEY(task_id, user_id)
+);
+
+CREATE TABLE task_bid_by (
+	task_id CHAR(8),
+	user_id VARCHAR(128),
+	amount NUMERIC,
+	
+	FOREIGN KEY (user_id) REFERENCES users(user_id)
+		ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (task_id) REFERENCES task_managed_by(task_id)
+		ON DELETE CASCADE ON UPDATE CASCADE,
+	
+	PRIMARY KEY(user_id, task_id)
+);
+
