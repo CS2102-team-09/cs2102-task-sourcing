fdec79cb9e72a267f898a94848c207643c3eaccc
Gerald Wong <e0003102@u.nus.edu>
2018-10-10 01:04:54 +0800

    Update tasks.php file

--- htdocs/tasks.php
+++ htdocs/tasks.php
@@ -14,72 +14,8 @@ if (!$query) {
 	<head>
 		<title>Tasks available</title>
 		<link href="style.css" rel="stylesheet" type="text/css">
-		<style type="text/css">
-		table {
-			margin: auto;
-			font-family: "Lucida Sans Unicode", "Lucida Grande", "Segoe Ui";
-			font-size: 12px;
-		}
-
-		table td {
-			transition: all .5s;
-		}
-		
-		/* Table */
-		.data-table {
-			border-collapse: collapse;
-			font-size: 14px;
-			min-width: 537px;
-		}
-
-		.data-table th, 
-		.data-table td {
-			border: 1px solid #e1edff;
-			padding: 7px 17px;
-		}
-		.data-table caption {
-			margin: 7px;
-		}
-
-		/* Table Header */
-		.data-table thead th {
-			background-color: #508abb;
-			color: #FFFFFF;
-			border-color: #6ea1cc !important;
-			text-transform: uppercase;
-		}
-
-		/* Table Body */
-		.data-table tbody td {
-			color: #353535;
-		}
-		.data-table tbody td:first-child,
-		.data-table tbody td:nth-child(4),
-		.data-table tbody td:last-child {
-			text-align: right;
-		}
-
-		.data-table tbody tr:nth-child(odd) td {
-			background-color: #f4fbff;
-		}
-		.data-table tbody tr:hover td {
-			background-color: #ffffa2;
-			border-color: #ffff0f;
-		}
-
-		/* Table Footer */
-		.data-table tfoot th {
-			background-color: #e5f5ff;
-			text-align: right;
-		}
-		.data-table tfoot th:first-child {
-			text-align: left;
-		}
-		.data-table tbody td:empty
-		{
-			background-color: #ffcccc;
-		}
-		</style>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	</head>
 		<body>
 			<div id="profile">
@@ -87,35 +23,45 @@ if (!$query) {
 				<b id="logout"><a href="logout.php">Log Out</a></b>
 			</div>
 			<h1>Tasks</h1>
-			<table>
-				<thead>
-				<tr>
-					<th>Title</th>
-					<th>Description</th>
-					<th>Start Time</th>
-					<th>End Time</th>
-					<th>Date of Task</th>
-					<th>Status</th>
-					<th>Owner</th>
-				</tr>
-				</thead>
-				<tbody>
-				<?php
-				$no 	= 1;
-				while ($row = pg_fetch_array($query)) {
-					echo '<tr>
-							<td>'.$row['task_title'].'</td>
-							<td>'.$row['description'].'</td>
-							<td>'.$row['start_time'].'</td>
-							<td>'.$row['end_time'].'</td>
-							<td>'. date('F d, Y', strtotime($row['date'])) . '</td>
-							<td>'.$row['status'].'</td>
-							<td>'.$row['user_id'].'</td>
-						</tr>';
-					$no++;
-				}
-				?>
-			</tbody>
-		</table>
+			<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+	        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+	        crossorigin="anonymous">
+	        </script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+	        	integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+	        	crossorigin="anonymous">
+        	</script>
+			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+	        	integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+	        	crossorigin="anonymous">
+        	</script>
 		</body>
+		<?php
+		$i = 0; 
+		// output data of each row
+	    while($row = pg_fetch_array($query)) {
+	        $i++;
+	        $task_owner = $row['user_id'];
+	        $task_title = $row["task_title"];
+	        $task_description = $row["description"];
+	        echo "
+	            <div class='accordion' id='accordion".$i."'>
+  					<div class='card'>
+					    <div class='card-header' id='headingOne".$i."'>
+					      <h5 class='mb-0'>
+					        <button class='btn btn-link' type='button' data-toggle='collapse' data-target='#collapseOne' aria-expanded='true' aria-controls='collapseOne'>".$task_title."
+					        </button>
+					      </h5>
+					    </div>
+
+					    <div id='collapseOne' class='collapse show' aria-labelledby='headingOne'>
+					      <div class='card-body'>".$task_description."
+					      </div>
+					    </div>
+					  </div>
+</div>
+	        ";
+	    }
+
+		?>
 </html>
