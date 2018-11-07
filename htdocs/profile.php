8f686dfc3508a54723d6243e2526010526bed045
caidicaidi123 <caidicaidi123@gmail.com>
2018-11-07 16:28:37 +0800

    refactor header/navbar

--- htdocs/profile.php
+++ htdocs/profile.php
@@ -1,5 +1,6 @@
 <?php
 include('session.php');
+include ('./components/profileHeader.html');
 ?>
 <!DOCTYPE html>
 <html>
@@ -13,35 +14,6 @@ include('session.php');
 
 </head>
 <body>
-
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">TaskSource</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <a class="nav-link active" href="profile.php"> Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="bids.php"> My Bids <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="createtask.php"> Create New <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="tasks.php"> All Tasks <span class="sr-only">(current)</span></a>
-            </li>
-        </ul>
-
-        <a class="btn btn-outline-success my-2 my-sm-0" href="logout.php">Logout</a>
-
-    </div>
-</nav>
-
 
 <div class="container h-100 d-flex justify-content-center" style="margin-top: 100px">
     <div class="jumbotron my-auto">
