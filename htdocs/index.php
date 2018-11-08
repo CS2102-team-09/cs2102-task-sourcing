39073a8892a2c73208b19e7d3a426d5f65e51af6
caidicaidi123 <caidicaidi123@gmail.com>
2018-11-08 13:46:00 +0800

    implement landing page

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,6 +1,6 @@
 <?php
 include('login.php'); // Includes Login Script
-include('./components/index_header.html');
+include('./components/index_header.php');
 
 if (isset($_SESSION['login_user'])) {
     header("location: profile.php");
